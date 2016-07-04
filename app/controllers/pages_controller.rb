3f2e9c43b27d9803b9a7e3c64b18b54d7cc8cc2a
Oscar Contreras <ocmchile@gmail.com>
2016-07-04 19:53:55 -0400

    con formulario funcionando

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def x
   end
+
+  def okregistro
+  	if params[:name].present? && params[:email].present? && params[:edad].present?
+	  	User.create(name: params[:name], email: params[:email], edad: params[:edad])
+	  	@users = User.all
+	  	redirect_to pages_x_path, notice: "El registro fue guardado"
+	else
+		redirect_to pages_x_path, notice: "DEBE INGRESAR TODOS LOS DATOS"
+  	end
+  end
 end
